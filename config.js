d2263503303db396f3569e5fdadc51efefcc0f54
my-son-pao <yazhoulongge@gmail.com>
2026-03-19 18:43:35 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const SUPABASE_URL = 'https://fcsjrxblyyjujxobblqq.supabase.co';
 
 // Project API keys → anon public（点 copy 复制）
-const SUPABASE_KEY = 'sb_publishable_Q5x_xCN9MbUWGerDUTe6Fw_tJaUjAGy';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjc2pyeGJseXlqdWp4b2JibHFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM5MDQ2MzUsImV4cCI6MjA4OTQ4MDYzNX0.wbt1UuDN5qQnBt05MvpFLZ3d0PS7FPIiBbe1Y8oLW00';
 
 // Storage 桶的名字（你在 Supabase Storage 里创建的桶叫什么就填什么）
 const BUCKET_NAME = 'Pictures';
