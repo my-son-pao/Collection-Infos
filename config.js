f7d2ce3b56ebbeafef20377d7e5b4d84ac664f9b
my-son-pao <yazhoulongge@gmail.com>
2026-03-19 18:31:00 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta http-equiv="Content-Style-Type" content="text/css">
+  <title></title>
+  <meta name="Generator" content="Cocoa HTML Writer">
+  <meta name="CocoaVersion" content="2685.4">
+  <style type="text/css">
+    p.p1 {margin: 0.0px 0.0px 0.0px 0.0px; font: 16.0px 'Helvetica Neue'; color: #141313; -webkit-text-stroke: #141313}
+    span.s1 {font-kerning: none}
+  </style>
+</head>
+<body>
+<p class="p1"><span class="s1">const SUPABASE_URL = 'https://fcsjrxblyyjujxobblqq.supabase.co';</span></p>
+<p class="p1"><span class="s1">const SUPABASE_KEY = 'sb_publishable_Q5x_xCN9MbUWGerDUTe6Fw_tJaUjAGy';</span></p>
+</body>
+</html>
